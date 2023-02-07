39fdf583c48ed8e0fdced1de3486ce9ccf39e72e
joaopborba <joaopborba95@hotmail.com>
2023-02-07 12:14:19 -0300

    Alterações

--- conversorDeMoedas/app.js
+++ conversorDeMoedas/app.js
@@ -1,11 +1,11 @@
-const calculateButton = document.getElementById('calculate-button');
+const calculateButton = document.getElementById("calculate-button");
 
-function mult (){
-    const valueDollar = document.getElementById('value-dollar').value;
-    const valueCotacao = document.getElementById('value-cotacao').value;
-    const result = document.getElementById('result');
-    const cotacao = valueDollar * valueCotacao;
-    result.textContent = `O resultado é: ${cotacao}`;
+function mult() {
+  const valueDollar = document.getElementById("value-dollar").value;
+  const valueCotacao = document.getElementById("value-cotacao").value;
+  const result = document.getElementById("result");
+  const cotacao = valueDollar * valueCotacao;
+  result.textContent = `O resultado é: R$${cotacao.toFixed(2)}`;
 }
 
 calculateButton.addEventListener("click", mult);
